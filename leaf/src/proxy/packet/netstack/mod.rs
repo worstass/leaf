189fdc869ee41bb83f7eea0b53bdaee73bf16778
alex <nobody@nowhere.com>
2021-12-25 23:45:23 +0800

    smol netstack

--- leaf/src/proxy/packet/netstack/mod.rs
+++ leaf/src/proxy/packet/netstack/mod.rs
@@ -1,4 +1,5 @@
 mod stack;
 mod stack_impl;
+mod endpoint;
 
 pub use stack::NetStack;
--- leaf/src/proxy/packet/netstack/stack_impl.rs
+++ leaf/src/proxy/packet/netstack/stack_impl.rs
@@ -9,9 +9,10 @@ use std::{
     },
     time,
 };
+use std::collections::BTreeMap;
 use log::*;
 use futures::task::{Context, Poll};
-use smoltcp::iface::InterfaceBuilder;
+use smoltcp::iface::{InterfaceBuilder, NeighborCache};
 use smoltcp::phy::{Medium, RawSocket};
 use tokio::{
     self,
@@ -23,7 +24,10 @@ use crate::{Dispatcher, NatManager};
 use crate::app::fake_dns::FakeDns;
 
 use smoltcp::Result;
-use smoltcp::socket::{TcpSocket, UdpSocket};
+use smoltcp::socket::{TcpSocket, TcpSocketBuffer, UdpPacketMetadata, UdpSocket, UdpSocketBuffer};
+use smoltcp::time::Instant;
+use smoltcp::wire::{IpAddress, IpCidr};
+use crate::proxy::packet::netstack::endpoint::Endpoint;
 
 pub struct NetStackImpl {
 
@@ -36,16 +40,47 @@ impl NetStackImpl {
         nat_manager: Arc<NatManager>,
         fakedns: Arc<TokioMutex<FakeDns>>,
     ) -> Box<Self> {
-        // let device = RawSocket::new("tun1", Medium::Ip).unwrap();
-        //
-        // let mut builder = InterfaceBuilder::new(device, vec![])
-        //     .any_ip(true)
-        //     .ip_addrs(ip_addrs);
-        // // builder = builder
-        // //     .hardware_addr(ieee802154_addr.into())
-        // //     .neighbor_cache(neighbor_cache);
-        // let mut iface = builder.finalize();
-        //
+        let device = Endpoint::new();
+
+        let ip_addrs = [
+            IpCidr::new(IpAddress::v4(192, 168, 69, 1), 24),
+        ];
+        let mut builder = InterfaceBuilder::new(device, vec![])
+            .any_ip(true)
+            .ip_addrs(ip_addrs);
+        let mut iface = builder.finalize();
+        let udp_rx_buffer = UdpSocketBuffer::new(vec![UdpPacketMetadata::EMPTY], vec![0; 64]);
+        let udp_tx_buffer = UdpSocketBuffer::new(vec![UdpPacketMetadata::EMPTY], vec![0; 128]);
+        let udp_socket = UdpSocket::new(udp_rx_buffer, udp_tx_buffer);
+        let tcp_rx_buffer = TcpSocketBuffer::new(vec![0; 64]);
+        let tcp_tx_buffer = TcpSocketBuffer::new(vec![0; 128]);
+        let tcp_socket = TcpSocket::new(tcp_rx_buffer, tcp_tx_buffer);
+
+        let udp_handle = iface.add_socket(udp_socket);
+        let tcp_handle = iface.add_socket(tcp_socket);
+
+        tokio::spawn(async move {
+            loop {
+                let timestamp = Instant::now();
+                match iface.poll(timestamp) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        debug!("poll error: {}", e);
+                    }
+                }
+
+                let socket = iface.get_socket::<UdpSocket>(udp_handle);
+                if !socket.is_open() {
+                    socket.bind(6969).unwrap()
+                }
+
+                let socket = iface.get_socket::<TcpSocket>(tcp_handle);
+                if !socket.is_open() {
+                    socket.listen(6969).unwrap();
+                }
+
+            }
+        });
         // let socket = iface.get_socket::<TcpSocket>(tcp_handle);
         // if socket.is_active() && !tcp_active {
         //     debug!("connected");
