92ad423c6b7a449a9e7a3bce8c54de20fe146375
nobody <nobody@nowhere.com>
2022-01-10 16:58:03 +0800

    restore tun

--- leaf/src/proxy/packet/inbound.rs
+++ leaf/src/proxy/packet/inbound.rs
@@ -25,6 +25,7 @@ use std::net::{SocketAddr, TcpStream};
 use std::os::unix::io::FromRawFd;
 use std::pin::Pin;
 use tokio::fs::File;
+use tokio::net::TcpListener;
 use tun::{AsyncDevice, TunPacket, TunPacketCodec};
 use crate::config::TunInboundSettings;
 
@@ -83,144 +84,108 @@ fn sink_from_pipe(pipe: &str) -> Result<Pin<Box<dyn Sink>>> {
     Ok(Box::pin(unsafe { File::from_raw_fd(21) }))
 }
 
+impl Sink for AsyncDevice {}
+
 pub fn new(
     inbound: Inbound,
     dispatcher: Arc<Dispatcher>,
     nat_manager: Arc<NatManager>,
 ) -> Result<Runner> {
-    let tun = sink_from_tun().unwrap();
-    work(inbound, dispatcher, nat_manager, tun)
-}
+    let settings = PacketInboundSettings::parse_from_bytes(&inbound.settings)?;
+    let port = settings.local_port;
+    // // FIXME it's a bad design to have 2 lists in config while we need only one
+    // let fake_dns_exclude = settings.fake_dns_exclude;
+    // let fake_dns_include = settings.fake_dns_include;
+    // if !fake_dns_exclude.is_empty() && !fake_dns_include.is_empty() {
+    //     return Err(anyhow!(
+    //         "fake DNS run in either include mode or exclude mode"
+    //     ));
+    // }
+    // let (fake_dns_mode, fake_dns_filters) = if !fake_dns_include.is_empty() {
+    //     (FakeDnsMode::Include, fake_dns_include)
+    // } else {
+    //     (FakeDnsMode::Exclude, fake_dns_exclude)
+    // };
+    let mut fakedns = FakeDns::new(FakeDnsMode::Exclude);
+    let fakedns = Arc::new(TokioMutex::new(fakedns));
+    // for filter in fake_dns_filters.into_iter() {
+    //     fakedns.add_filter(filter);
+    // }
+    // let tun = sink_from_tun(&inbound.settings).unwrap();
+    // let stack =  NetStack::new(inbound.tag, dispatcher, nat_manager, Arc::new(TokioMutex::new(fakedns)));
+    // let stack = Arc::new(stack);
 
-impl Sink for AsyncDevice {}
-
-// fn sink_from_tun(bytes: &[u8]) -> Result<Pin<Box<dyn Sink>>> {
-//     let settings = TunInboundSettings::parse_from_bytes(bytes)?;
-//     let mut cfg = tun::Configuration::default();
-//     if settings.fd >= 0 {
-//         cfg.raw_fd(settings.fd);
-//     } else if settings.auto {
-//         cfg.name(&*option::DEFAULT_TUN_NAME)
-//             .address(&*option::DEFAULT_TUN_IPV4_ADDR)
-//             .destination(&*option::DEFAULT_TUN_IPV4_GW)
-//             .mtu(1500);
-//
-//         #[cfg(not(any(
-//         target_arch = "mips",
-//         target_arch = "mips64",
-//         target_arch = "mipsel",
-//         target_arch = "mipsel64",
-//         )))]
-//             {
-//                 cfg.netmask(&*option::DEFAULT_TUN_IPV4_MASK);
-//             }
-//
-//         cfg.up();
-//     } else {
-//         cfg.name(settings.name)
-//             .address(settings.address)
-//             .destination(settings.gateway)
-//             .mtu(settings.mtu);
-//
-//         #[cfg(not(any(
-//         target_arch = "mips",
-//         target_arch = "mips64",
-//         target_arch = "mipsel",
-//         target_arch = "mipsel64",
-//         )))]
-//             {
-//                 cfg.netmask(settings.netmask);
-//             }
-//
-//         cfg.up();
-//     }
-//
-//     let dev = tun::create_as_async(&cfg).unwrap();
-//     Ok(Box::pin(dev))
-// }
-
-
-fn work<Tun>(
-    inbound: Inbound,
-    dispatcher: Arc<Dispatcher>,
-    nat_manager: Arc<NatManager>,
-    tun: Tun,
-) -> Result<Runner>
-    where Tun: AsyncRead + AsyncWrite + Send + 'static
-{
-    let settings = TunInboundSettings::parse_from_bytes(&inbound.settings)?;
-    // FIXME it's a bad design to have 2 lists in config while we need only one
-    let fake_dns_exclude = settings.fake_dns_exclude;
-    let fake_dns_include = settings.fake_dns_include;
-    if !fake_dns_exclude.is_empty() && !fake_dns_include.is_empty() {
-        return Err(anyhow!(
-            "fake DNS run in either include mode or exclude mode"
-        ));
-    }
-    let (fake_dns_mode, fake_dns_filters) = if !fake_dns_include.is_empty() {
-        (FakeDnsMode::Include, fake_dns_include)
-    } else {
-        (FakeDnsMode::Exclude, fake_dns_exclude)
-    };
     Ok(Box::pin(async move {
-        let fakedns = Arc::new(TokioMutex::new(FakeDns::new(fake_dns_mode)));
+        let listen_addr = format!("127.0.0.1:{}", port);
+        let listener = TcpListener::bind(&listen_addr).await.unwrap();
+        info!("packet inbound listening tcp {}", &listen_addr);
+        loop {
+            match listener.accept().await {
+                Ok((stream, _)) => {
+                    let dispatcher = dispatcher.clone();
+                    let nat_manager = nat_manager.clone();
+                    let fakedns = fakedns.clone();
+                    let tag = inbound.clone().tag;
+                    let stack =  NetStack::new(tag, dispatcher, nat_manager, fakedns);
+                    let (mut stack_reader, mut stack_writer) = io::split(stack);
+                    // let tun = sink_from_tun().unwrap();
+                    let tun = stream;
+                    let pi = true;
+                    let mtu = 1504;
+                    let codec = TunPacketCodec::new(pi, mtu);
+                    let framed = Framed::new(tun, codec);
+                    let (mut tun_sink, mut tun_stream) = framed.split();
 
-        for filter in fake_dns_filters.into_iter() {
-            fakedns.lock().await.add_filter(filter);
+                    let s2t = Box::pin(async move {
+                        let mut buf = vec![0; mtu as usize];
+                        loop {
+                            match stack_reader.read(&mut buf).await {
+                                Ok(0) => {
+                                    debug!("read stack eof");
+                                    return;
+                                }
+                                Ok(n) => match tun_sink.send(TunPacket::new((&buf[..n]).to_vec())).await {
+                                    Ok(_) => (),
+                                    Err(e) => {
+                                        warn!("send pkt to tun failed: {}", e);
+                                        return;
+                                    }
+                                },
+                                Err(err) => {
+                                    warn!("read stack failed {:?}", err);
+                                    return;
+                                }
+                            }
+                        }
+                    });
+
+                    let t2s = Box::pin(async move {
+                        while let Some(packet) = tun_stream.next().await {
+                            match packet {
+                                Ok(packet) => match stack_writer.write(packet.get_bytes()).await {
+                                    Ok(_) => (),
+                                    Err(e) => {
+                                        warn!("write pkt to stack failed: {}", e);
+                                        return;
+                                    }
+                                },
+                                Err(err) => {
+                                    warn!("read tun failed {:?}", err);
+                                    return;
+                                }
+                            }
+                        }
+                    });
+
+                    info!("packet inbound started");
+                    futures::future::select(t2s, s2t).await;
+                    info!("packet inbound exited");
+                }
+                Err(e) => {
+                    error!("accept connection failed: {}", e);
+                    break;
+                }
+            }
         }
-
-        let stack = NetStack::new(inbound.tag, dispatcher, nat_manager, fakedns);
-        let (mut stack_reader, mut stack_writer) = io::split(stack);
-
-        let pi = true;
-        let mtu = 1504;
-        let codec = TunPacketCodec::new(pi, mtu);
-        let framed = Framed::new(tun, codec);
-        let (mut tun_sink, mut tun_stream) = framed.split();
-
-        let s2t = Box::pin(async move {
-            let mut buf = vec![0; mtu as usize];
-            loop {
-                match stack_reader.read(&mut buf).await {
-                    Ok(0) => {
-                        debug!("read stack eof");
-                        return;
-                    }
-                    Ok(n) => match tun_sink.send(TunPacket::new((&buf[..n]).to_vec())).await {
-                        Ok(_) => (),
-                        Err(e) => {
-                            warn!("send pkt to tun failed: {}", e);
-                            return;
-                        }
-                    },
-                    Err(err) => {
-                        warn!("read stack failed {:?}", err);
-                        return;
-                    }
-                }
-            }
-        });
-
-        let t2s = Box::pin(async move {
-            while let Some(packet) = tun_stream.next().await {
-                match packet {
-                    Ok(packet) => match stack_writer.write(packet.get_bytes()).await {
-                        Ok(_) => (),
-                        Err(e) => {
-                            warn!("write pkt to stack failed: {}", e);
-                            return;
-                        }
-                    },
-                    Err(err) => {
-                        warn!("read tun failed {:?}", err);
-                        return;
-                    }
-                }
-            }
-        });
-
-        info!("tun inbound started");
-        futures::future::select(t2s, s2t).await;
-        info!("tun inbound exited");
     }))
 }
