1db0efa1e6463a5bc9479075dc202606a46ea6e3
nobody <nobody@nowhere.com>
2022-01-21 21:27:03 +0800

    fix build for apple

--- leaf-darwin/leaf.swift
+++ leaf-darwin/leaf.swift
@@ -10,22 +10,17 @@ public func runWithOptions(
     threads: Int32,
     stackSize: Int32
 ) -> Int32 {
-//    let p = UnsafeMutablePointer<CChar>(mutating: configPath.cString(using: .utf8))
     return leaf_run_with_options(rtId, configPath.cString(using: .utf8), autoReload, multiThread, autoTheads, threads, stackSize);
 }
 
-public func run(rtId: UInt16, configPath: String)  -> Int32 {
-    let a = configPath.cString(using: .utf8)
-   
-//    let p = UnsafeMutablePointer<CChar>(mutating: a)
-    return leaf_run(rtId, a)
+public func run(rtId: UInt16, configPath: String) -> Int32 {
+    return leaf_run(rtId, configPath.cString(using: .utf8))
 }
 
-public func reload(rtId: UInt16)  -> Int32 {
+public func reload(rtId: UInt16) -> Int32 {
     return leaf_reload(rtId)
 }
 
-public func shutdown(rtId: UInt16)  -> Bool {
+public func shutdown(rtId: UInt16) -> Bool {
     return leaf_shutdown(rtId)
 }
-
