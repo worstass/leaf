9f10d63d402c9c4126d0d5b284bed555b7db4a30
nobody <nobody@nowhere.com>
2022-03-08 10:34:05 +0800

    ffi: add callback struct create and destroy

--- leaf-ffi/src/callback.rs
+++ leaf-ffi/src/callback.rs
@@ -2,16 +2,41 @@ use std::fmt::{Debug, Formatter};
 use leaf::callback::Callback as Inner;
 use std::option::Option;
 use std::os::raw::{c_float, c_ulonglong};
+use std::ptr::null;
+
+type ReportTrafficFunc = Option<extern "C" fn(
+    tx_rate: c_float,
+    rx_rate: c_float,
+    tx_total: c_ulonglong,
+    rx_total: c_ulonglong,
+) -> ()>;
 
 #[repr(C)]
 pub struct Callback {
-    pub report_traffic: Option<extern "C" fn(
-        tx_rate: c_float,
-        rx_rate: c_float,
-        tx_total: c_ulonglong,
-        rx_total: c_ulonglong,
-    ) -> ()>,
+    pub report_traffic: ReportTrafficFunc,
+    // Option<extern "C" fn(
+    //     tx_rate: c_float,
+    //     rx_rate: c_float,
+    //     tx_total: c_ulonglong,
+    //     rx_total: c_ulonglong,
+    // ) -> ()>,
 }
+
+#[no_mangle]
+pub extern "C" fn create_callback(
+    report_traffic: ReportTrafficFunc,
+    // Option<extern "C" fn(
+    //     tx_rate: c_float,
+    //     rx_rate: c_float,
+    //     tx_total: c_ulonglong,
+    //     rx_total: c_ulonglong,
+    // ) -> ()>,
+) -> *const Callback {
+    return null();
+}
+
+#[no_mangle]
+pub extern "C" fn destroy_callback(cb: *const Callback) {}
 
 pub(crate) struct FfiCallback {
     inner: *const Callback,
@@ -38,7 +63,7 @@ unsafe impl Sync for FfiCallback {}
 impl Inner for FfiCallback {
     fn report_traffic(self: &Self, tx_rate: f32, rx_rate: f32, rx_total: u64, tx_total: u64) {
         unsafe {
-            let f = (* self.inner).report_traffic.unwrap();
+            let f = (*self.inner).report_traffic.unwrap();
             f(tx_rate as c_float, rx_rate as c_float, rx_total as c_ulonglong, tx_total as c_ulonglong);
         }
     }
