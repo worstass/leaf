3151f5d2c5a0f51eb5ca3434689ef802eca2b405
nobody <nobody@nowhere.com>
2022-03-16 00:09:56 +0800

    packet ok

--- leaf/src/proxy/packet/inbound.rs
+++ leaf/src/proxy/packet/inbound.rs
@@ -40,12 +40,12 @@ pub fn new(
     let settings = PacketInboundSettings::parse_from_bytes(&inbound.settings)?;
     let port = settings.port;
 
-    #[cfg(any(target_os = "linux", target_os = "macos"))]
-    let pakcet_has_pi = settings.pi;
-    #[cfg(any(target_os = "ios"))]
-    let pakcet_has_pi = true;
+    #[cfg(any(target_os = "linux"))]
+        let pakcet_has_pi = settings.pi;
+    #[cfg(any(target_os = "ios", target_os = "macos"))]
+        let pakcet_has_pi = false;
     #[cfg(any(target_os = "windows", target_os = "android"))]
-    let pakcet_has_pi =  false;
+        let pakcet_has_pi = false;
 
     // // FIXME it's a bad design to have 2 lists in config while we need only one
     let fake_dns_exclude = settings.fake_dns_exclude;
@@ -69,7 +69,7 @@ pub fn new(
         info!("packet inbound listening tcp {}", &listen_addr);
         loop {
             match listener.accept().await {
-                Ok((stream, _)) => {
+                Ok((mut stream, _)) => {
                     let dispatcher = dispatcher.clone();
                     let nat_manager = nat_manager.clone();
                     let fakedns = fakedns.clone();
@@ -78,10 +78,7 @@ pub fn new(
                     let (mut stack_reader, mut stack_writer) = io::split(stack);
                     // let pi = has_packet_information();
                     let mtu = 1504;
-                    let codec = TunPacketCodec::new(pakcet_has_pi, mtu);
-                    let framed = Framed::new(stream, codec);
-                    let (mut tun_sink, mut tun_stream) = framed.split();
-
+                    let (mut stream_reader, mut stream_writer) = stream.split();
                     let s2t = Box::pin(async move {
                         let mut buf = vec![0; mtu as usize];
                         loop {
@@ -91,15 +88,15 @@ pub fn new(
                                     return;
                                 }
                                 Ok(n) => {
-                                    debug!("stack->tun:{:02X?}", &buf[..n]);
-                                    match tun_sink.send(TunPacket::new((&buf[..n]).to_vec())).await {
+                                    debug!("stack->tcp:{:02X?}", &buf[..n]);
+                                    match stream_writer.write(&buf[..n]).await {
                                         Ok(_) => (),
                                         Err(e) => {
-                                            warn!("send pkt to tun failed: {}", e);
+                                            warn!("send pkt to tcp failed: {}", e);
                                             return;
                                         }
                                     }
-                                },
+                                }
                                 Err(err) => {
                                     warn!("read stack failed {:?}", err);
                                     return;
@@ -107,27 +104,110 @@ pub fn new(
                             }
                         }
                     });
-
                     let t2s = Box::pin(async move {
-                        while let Some(packet) = tun_stream.next().await {
-                            match packet {
-                                Ok(packet) => {
-                                    debug!("tun->stack:{:02X?}", packet.get_bytes());
-                                    match stack_writer.write(packet.get_bytes()).await {
-                                        Ok(_) => (),
+                        let mut packet_header_buf = vec![0; 6];
+                        let mut packet_buf = vec![0u8; mtu];
+                        loop {
+                            match stream_reader.read_exact(&mut packet_header_buf).await {
+                                Ok(n) => {
+                                    if n < 6 {
+                                        warn!("read <4 bytes, n={}", n);
+                                        return;
+                                    }
+                                    let ver = packet_header_buf[0]>>4;
+                                    let pkt_len =  match ver {
+                                        4=> {
+                                            packet_header_buf[2] as usize * 256 + packet_header_buf[3] as usize
+                                        },
+                                        6=> {
+                                            let payload_len = packet_header_buf[4] as usize * 256 + packet_header_buf[5] as usize;
+                                            payload_len + 40
+                                        }
+                                        _=> {
+                                            warn!("packet ver wrong ver={}", ver);
+                                            return;
+                                        }
+                                    };
+                                    let need_to_read = pkt_len-6;
+                                    match stream_reader.read_exact(&mut packet_buf[..need_to_read]).await {
+                                        Ok(n) => {
+                                            if n < need_to_read {
+                                                warn!("read packet body <len={}", n);
+                                                return;
+                                            }
+                                            let pkt = [&packet_header_buf[..], &packet_buf[..n]].concat();
+                                            debug!("tcp->stack:{:02X?}", &pkt[..]);
+                                            match stack_writer.write(&pkt[..]).await {
+                                                Ok(_) => (),
+                                                Err(e) => {
+                                                    warn!("write pkt to stack failed: {}", e);
+                                                    return;
+                                                }
+                                            }
+                                        }
                                         Err(e) => {
-                                            warn!("write pkt to stack failed: {}", e);
+                                            warn!("read packet body from tcp failed {:?}", e);
                                             return;
                                         }
                                     }
-                                },
-                                Err(err) => {
-                                    warn!("read tun failed {:?}", err);
+                                }
+                                Err(e) => {
+                                    warn!("read packet head from tcp failed {:?}", e);
                                     return;
                                 }
                             }
                         }
                     });
+                    // let codec = TunPacketCodec::new(pakcet_has_pi, mtu);
+                    // let framed = Framed::new(stream, codec);
+                    // let (mut tun_sink, mut tun_stream) = framed.split();
+                    //
+                    // let s2t = Box::pin(async move {
+                    //     let mut buf = vec![0; mtu as usize];
+                    //     loop {
+                    //         match stack_reader.read(&mut buf).await {
+                    //             Ok(0) => {
+                    //                 debug!("read stack eof");
+                    //                 return;
+                    //             }
+                    //             Ok(n) => {
+                    //                 debug!("stack->tun:{:02X?}", &buf[..n]);
+                    //                 match tun_sink.send(TunPacket::new((&buf[..n]).to_vec())).await {
+                    //                     Ok(_) => (),
+                    //                     Err(e) => {
+                    //                         warn!("send pkt to tun failed: {}", e);
+                    //                         return;
+                    //                     }
+                    //                 }
+                    //             },
+                    //             Err(err) => {
+                    //                 warn!("read stack failed {:?}", err);
+                    //                 return;
+                    //             }
+                    //         }
+                    //     }
+                    // });
+                    //
+                    // let t2s = Box::pin(async move {
+                    //     while let Some(packet) = tun_stream.next().await {
+                    //         match packet {
+                    //             Ok(packet) => {
+                    //                 debug!("tun->stack:{:02X?}", packet.get_bytes());
+                    //                 match stack_writer.write(packet.get_bytes()).await {
+                    //                     Ok(_) => (),
+                    //                     Err(e) => {
+                    //                         warn!("write pkt to stack failed: {}", e);
+                    //                         return;
+                    //                     }
+                    //                 }
+                    //             },
+                    //             Err(err) => {
+                    //                 warn!("read tun failed {:?}", err);
+                    //                 return;
+                    //             }
+                    //         }
+                    //     }
+                    // });
 
                     info!("packet inbound started");
                     futures::future::select(t2s, s2t).await;
