497017f42ba51feeb030b12f13beb4ef18b0bfb7
unknown <nobody@nowhere.com>
2023-04-04 00:15:49 +0800

    use ncap lib

--- leaf-uwp/register.cmd
+++ leaf-uwp/register.cmd
@@ -1,4 +1,4 @@
-cargo build -Z build-std=std,panic_abort --target x86_64-uwp-windows-msvc --release
+cargo build -Z build-std=std,panic_abort --target aarch64-uwp-windows-msvc
 copy appx\* ..\target\debug
 cd ..\target\debug
 powershell -command "Get-AppxPackage LeafVpn | Remove-AppxPackage"
